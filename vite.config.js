e898ce1caa25ffdd021f6eb49a54a6404287277a
Krishna Manda <krishna.manda@gmail.com>
2026-02-15 08:26:24 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/garden/',  // Change this to match your GitHub Pages path
+  base: '/',  // Change this to match your GitHub Pages path
 })
